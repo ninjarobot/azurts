18d0ce2711d5c1f2ae380761b846a92cf8641e8d
Dave Curylo <curylod@asme.org>
2019-12-06 09:39:03 -0500

    Add broadcast hook

--- azurts/Hook.fs
+++ azurts/Hook.fs
@@ -6,3 +6,13 @@ let compose (first: 'a -> 'b option) (second: 'b -> 'c option) : 'a -> 'c option
     fun input -> input |> first |> Option.bind second
 
 let (>=>) = compose
+
+/// Sends copy of the input to multiple hooks.
+let broadcast (hooks: Hook<'a, Async<unit>> list) =
+    fun input ->
+        hooks
+        |> List.map (fun hook -> input |> hook)
+        |> List.choose id
+        |> function
+        | [] -> None
+        | filteredHooks -> filteredHooks |> Async.Parallel |> Async.Ignore |> Some
