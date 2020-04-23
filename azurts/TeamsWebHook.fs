1fda51c51c4d08e08a20bbef66c2ae8a999d6c3b
Dave Curylo <curylod@asme.org>
2020-04-23 10:04:55 -0400

    Build a Teams card from a Log Alert.

--- azurts/TeamsWebHook.fs
+++ azurts/TeamsWebHook.fs
@@ -1,6 +1,7 @@
 module azurts.TeamsWebHook
 
 open System
+open System.Net.Http
 open Chiron
 
 module ContentTypes =
@@ -92,3 +93,114 @@ type MessageCard =
             do! Json.write "sections" messageCard.Sections
             do! Json.writeUnlessDefault "potentialAction" [] messageCard.PotentialActions
         }
+
+type TeamsWebHookConfig =
+    {
+        Client : HttpClient
+        WebHookUri : System.Uri
+        ErrorCallback : System.Net.HttpStatusCode * string -> unit
+    }
+
+module MessageCard =
+    let format (messageCard:MessageCard) =
+        messageCard |> Json.serialize |> Json.formatWith JsonFormattingOptions.Pretty
+    
+    let private toTitleCase = System.Globalization.CultureInfo.InvariantCulture.TextInfo.ToTitleCase
+    
+    let dataFromJson (json:Chiron.Json) =
+        match json with
+        | Chiron.Json.String s -> s
+        | Chiron.Json.Number n -> string n
+        | Chiron.Json.Bool b -> string b
+        | Chiron.Json.Null _ -> String.Empty
+        | json -> Json.format json
+    
+    let severity (severity:int) =
+        if severity = 0 then "Critical"
+        elif severity = 1 then "Error"
+        elif severity = 2 then "Warning"
+        else "Information"
+
+    let ofAzureAlert (channel:string) (alert:AzureAlert.LogAlert) : MessageCard seq option =
+        let alertAction = Action.OpenUri (Name="View Logs", Targets=[{Os=OpenUriTargetOS.Default; Uri=alert.Data.LinkToSearchResults |> Uri}])
+        let activityTitle = alert.Data.Severity |> severity
+        let activitySubtitle = String.Format ("{0} to {1}",
+                                              alert.Data.SearchIntervalStartTime,
+                                              alert.Data.SearchIntervalEndTime)
+        alert.Data.SearchResult.Tables |> List.tryHead |> Option.map
+            (
+            fun table ->
+                seq {
+                    if table.Rows.Length > 0 then
+                        for row in table.Rows do
+                            let item = Seq.zip table.Columns row
+                            let facts =
+                                item
+                                |> Seq.filter (fun (column, _) -> column.Name <> "message")
+                                |> Seq.filter (fun (column, row) -> match row with | Json.Null _ -> false | _ -> true )
+                                |> Seq.map (fun (column, row) -> { Name = column.Name.Replace("customDimensions_", ""); Value = dataFromJson row })
+                            let message = item |> Seq.tryFind (fun (column, _) -> column.Name = "message") |> Option.map (fun (_, row) -> dataFromJson row)
+                            {
+                                ThemeColor = "0076D7"
+                                Summary = alert.Data.AlertRuleName
+                                PotentialActions = [ alertAction ]
+                                Sections =
+                                    [
+                                        yield
+                                            {
+                                                Title = None
+                                                StartGroup = None
+                                                ActivityTitle = Some activityTitle
+                                                ActivitySubtitle = Some activitySubtitle
+                                                ActivityImage = None
+                                                ActivityText = alert.Data.Description |> Option.ofObj
+                                                Facts = Some (facts |> List.ofSeq)
+                                                Text = None
+                                            }
+                                        if message.IsSome then
+                                            yield
+                                                {
+                                                    Title = None
+                                                    StartGroup = None
+                                                    ActivityTitle = None
+                                                    ActivitySubtitle = None
+                                                    ActivityImage = None
+                                                    ActivityText = None
+                                                    Facts = None
+                                                    Text = Some (String.Format("""<pre>{0}</pre>""", (System.Net.WebUtility.HtmlEncode(message.Value))))
+                                                }
+                                    ]
+                            }
+                    else
+                        {
+                            ThemeColor = "0076D7"
+                            Summary = alert.Data.AlertRuleName
+                            PotentialActions = [ alertAction ]
+                            Sections =
+                                [
+                                    yield
+                                        {
+                                            Title = None
+                                            StartGroup = None
+                                            ActivityTitle = Some activityTitle
+                                            ActivitySubtitle = Some activitySubtitle
+                                            ActivityImage = None
+                                            ActivityText = alert.Data.Description |> Option.ofObj
+                                            Facts = None
+                                            Text = None
+                                        }
+                                ]
+                        }
+                }
+            )
+    
+    let sendToTeams (config:TeamsWebHookConfig) (messageCards:MessageCard seq) =
+        async {
+            for messageCard in messageCards do
+                use content = new StringContent (messageCard |> format, System.Text.Encoding.UTF8, ContentTypes.TeamsCardO365Connector)
+                use request = new HttpRequestMessage (HttpMethod.Post, config.WebHookUri, Content=content)
+                use! response = request |> config.Client.SendAsync |> Async.AwaitTask
+                if not response.IsSuccessStatusCode then
+                    let! responseBody = response.Content.ReadAsStringAsync () |> Async.AwaitTask
+                    config.ErrorCallback (response.StatusCode, responseBody)
+        } |> Some
